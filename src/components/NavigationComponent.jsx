ffa9bf4946990f98c5faf3ee2dda424c423d0014
Chanchal Kumar Mishra <chanchalmishra1947@gmail.com>
2022-05-28 13:08:13 +0530

    links

--- src/components/NavigationComponent.jsx
+++ src/components/NavigationComponent.jsx
@@ -1,38 +1,73 @@
-import React, { useState } from 'react';
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavLink, NavItem } from 'reactstrap';
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavLink,
+  NavItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 const NavigationComponent = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => setIsOpen(!isOpen);
 
-    return (
-        <div>
-            <Navbar dark expand="md" className="px-5 bg-1">
-                <NavbarBrand href="/">Alstack</NavbarBrand>
-                <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav navbar>
-                        <NavItem>
-                            <NavLink href="/">Home</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/bubbleSort">Bubble Sort</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/insertionSort">Insertion Sort</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/selectionSort">Selection Sort</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/binarySearchTree">Binary Search Tree</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Navbar dark expand="md" className="px-5 bg-1">
+        <NavbarBrand>
+          <Link className="text-light" to="/">
+            Alstack
+          </Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink>
+                <Link className="text-light" to="/">
+                  Home
+                </Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link className="text-light" to="/bubbleSort">
+                  Bubble Sort
+                </Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link className="text-light" to="/insertionSort">
+                  {" "}
+                  Insertion Sort
+                </Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link className="text-light" to="/selectionSort">
+                  Selection Sort
+                </Link>{" "}
+              </NavLink>
+            </NavItem>
+            {/* <NavItem>
+              <NavLink>
+                {" "}
+                <Link className="text-light" to="/binarySearchTree">
+                  Binary Search Tree
+                </Link>{" "}
+              </NavLink>
+            </NavItem> */}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
 export default NavigationComponent;
